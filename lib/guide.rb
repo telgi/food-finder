30444657096099ad2ab603d7d4c3b5997c0e06c8
Terry Allitt <tallitt88@gmail.com>
2017-12-27 14:34:31 +0000

    Create guide file with introduction and conclusion methods

---
+++ lib/guide.rb
@@ -0,0 +1,29 @@
+class Guide
+
+# passing file path as an argument allows for
+# flexibility in creating additional guides
+  def initialize(path=nil)
+    # locate the restaurant text file at path
+    # or create a new file
+    # exit if create fails
+  end
+
+  def launch!
+    introduction
+    # action loop
+      # what do you want to do? (list, add, find, quit)
+      # do that action
+    # repeat until user quits
+    conclusion
+  end
+
+  def introduction
+    puts "\n\n<<< Welcome to the Food Finder! >>>\n\n"
+    puts "This is an interactive guide to help you find the food you crave\n\n"
+  end
+
+  def conclusion
+    puts "\n<<< Goodbye and Bon Appetit! >>>\n\n\n"
+  end
+
+end
