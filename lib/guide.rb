ac11b37e9bb690d18978d4c416204d982db62d0c
Terry Allitt <tallitt88@gmail.com>
2017-12-29 16:23:48 +0000

    Add functionality to finding a restaurant by keyword

--- lib/guide.rb
+++ lib/guide.rb
@@ -28,8 +28,8 @@ class Guide
     # action loop
     result = nil
     until result == :quit
-      action = get_action
-      result = do_action(action)
+      action, args = get_action
+      result = do_action(action, args)
     end
     conclusion
   end
@@ -40,17 +40,19 @@ class Guide
       puts "Actions: " + Guide::Config.actions.join(", ") if action
       print "> "
       user_response = gets.chomp
-      action = user_response.downcase.strip
+      args = user_response.downcase.strip.split(" ")
+      action = args.shift
     end
-    return action
+    return action, args
   end
 
-  def do_action(action)
+  def do_action(action, args=[])
     case action
     when 'list'
       list
     when 'find'
-      puts "Finding..."
+      keyword = args.shift
+      find(keyword)
     when 'add'
       add
     when 'quit'
@@ -64,6 +66,22 @@ class Guide
     output_action_header("Listing restaurants")
     restaurants = Restaurant.saved_restaurants
     output_restaurant_table(restaurants)
+  end
+
+  def find(keyword="")
+    output_action_header("Find a restaurant")
+    if keyword
+      restaurants = Restaurant.saved_restaurants
+      found = restaurants.select do |rest|
+        rest.name.downcase.include?(keyword.downcase)    ||
+        rest.cuisine.downcase.include?(keyword.downcase) ||
+        rest.price.to_i <= keyword.to_i
+      end
+      output_restaurant_table(found)
+    else
+      puts "Find using a key phrase to search the restaurant list."
+      puts "Examples: 'find burger', 'find burg' etc.\n\n"
+    end
   end
 
   def add
